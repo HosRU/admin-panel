76ecef97b336d93f196d37ec56475d2dec0abaad
Artemounchik <Artem.fak@list.ru>
2024-04-27 18:50:11 +0300

    New update

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+const bodyParser = require("body-parser");
+app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute,
+    gamesRouter
+  ) 
+
+app.listen(PORT, () => {
+    console.log("Работаем")
+})
